ac38f64d7702449807147889749c73a15852f770
TylernJR <Tylern@wp.pl>
2020-11-30 14:33:09 +0100

    .

--- src/cwiczenia3/funkcja.java
+++ src/cwiczenia3/funkcja.java
@@ -1,6 +1,25 @@
 package cwiczenia3;
-//funkcje kwadratowe
+
 public class funkcja {
+
+    public static int rekur(int x){
+        if (x > 0){
+            return rekur(x-1);
+        }
+        return x;
+    }
+
+    public static int fibb(int x){
+        if(x == 0) return 0;
+        if(x == 1) return 1;
+        if (x >1)
+        return fibb(x-1) + fibb(x-2);
+        else{
+            return 0;
+        }
+
+    }
+
     public static float funkcja_kwadratowa(float x){
         return 2*x*x - 3*x +4;
     }
@@ -14,10 +33,11 @@ public class funkcja {
     }
 
     public static void main(String[] args){
-        System.out.println(funkcja_kwadratowa(5));
+//        System.out.println(funkcja_kwadratowa(5));
+//        int[] tab = {1,4,6,8,9,3,6,9,3,7,9,3};
+//        print_table(tab);
 
-        int[] tab = {1,4,6,8,9,3,6,9,3,7,9,3};
-
-        print_table(tab);
+        System.out.println((fibb(9)));
     }
+
 }
