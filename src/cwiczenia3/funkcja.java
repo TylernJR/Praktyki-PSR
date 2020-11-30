417be3c4937ed520eea0b2291912c1a4576c142f
TylernJR <Tylern@wp.pl>
2020-11-30 13:45:50 +0100

    funkcje

---
+++ src/cwiczenia3/funkcja.java
@@ -0,0 +1,12 @@
+package cwiczenia3;
+
+public class funkcja {
+    public static float funkcja_kwadratowa(float x){
+        return 2*x*x - 3*x +4;
+    }
+
+
+    public static void main(String[] args){
+        System.out.println(funkcja_kwadratowa(5));
+    }
+}
