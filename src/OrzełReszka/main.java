308df805fec104851db8bbfcd8551c9a890d5d62
TylernJR <Tylern@wp.pl>
2020-12-10 23:47:19 +0100

    .

---
+++ src/OrzełReszka/main.java
@@ -0,0 +1,12 @@
+package OrzełReszka;
+
+public class main {
+
+    public static void main(String[] args){
+
+        if (Math.random() < 0.5) System.out.println("Orzel");
+        else System.out.println("Reszka");
+    }
+    
+}
+
