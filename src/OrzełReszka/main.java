3b89cd0dfa262972d10dd2eb64a478813b7e4460
TylernJR <Tylern@wp.pl>
2020-12-13 18:27:37 +0100

    .

--- src/OrzełReszka/main.java
+++ src/OrzełReszka/main.java
@@ -4,6 +4,9 @@ public class main {
 
     public static void main(String[] args){
 
+        //math.random opiera się na wyborze od 0 do 1. dlatego jak damy 0.5 to będzie 50% szansy na jedno albo drugie.
+        //potem dajemy po IF, ELSE, czyli jeżeli nie orzeł, to reszka. i chyba tylko tyle mogę tu wyjaśnić.
+        //Po prostu math.random() < 0.5 oznacza ze będzie randomowe po 50 % wybory pomiędzy orłem a reszką.
         if (Math.random() < 0.5) System.out.println("Orzel");
         else System.out.println("Reszka");
     }
